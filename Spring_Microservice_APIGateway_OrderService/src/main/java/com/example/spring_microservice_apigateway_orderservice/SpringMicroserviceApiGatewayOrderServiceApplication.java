f9e34cd7df2c5fd74fae8c1f27e328c215eed65a
Zoe <zoe.vo@emagegroup.com>
2022-10-31 00:45:45 +0700

    init project

---
+++ Spring_Microservice_APIGateway_OrderService/src/main/java/com/example/spring_microservice_apigateway_orderservice/SpringMicroserviceApiGatewayOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_microservice_apigateway_orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceApiGatewayOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceApiGatewayOrderServiceApplication.class, args);
+    }
+
+}
---
+++ Spring_Microservice_APIGateway_OrderService/src/test/java/com/example/spring_microservice_apigateway_orderservice/SpringMicroserviceApiGatewayOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_microservice_apigateway_orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceApiGatewayOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
